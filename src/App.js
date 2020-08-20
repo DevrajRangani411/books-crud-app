50216bc334e21b1c00cf6d344cdd4cc2c7004cb2
DevrajRangani <devraj.rangani411@gmail.com>
2020-08-20 16:32:50 +0530

    Final Changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./Store/store";
-import Home from "./Component/Home";
 import Books from "./Component/Books";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        {/* With out Redux-Saga */}
-        {/* <Home/> */}
-
         {/* With Redux-Saga */}
         <Books />
       </div>
